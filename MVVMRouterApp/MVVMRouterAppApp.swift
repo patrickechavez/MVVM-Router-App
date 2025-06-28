de703da0370c85bb099c4c8a131fcc566231c657
John Patrick Echavez <johnpatrick.echavez@coreproc.com>
2025-06-28 17:21:35 +0800

    Initial Commit

---
+++ MVVMRouterApp/MVVMRouterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMRouterAppApp.swift
+//  MVVMRouterApp
+//
+//  Created by John Patrick Echavez on 6/28/25.
+//
+
+import SwiftUI
+
+@main
+struct MVVMRouterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
